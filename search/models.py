6735b6352344c1ad8d304cd5c49a95b42c78c976
USER <none>
2024-09-27 10:42:45 +0800

    20240927-database

--- search/models.py
+++ search/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
 # Create your models here.
+### 演員、劇院資料
 
-class movies(models.Model):
-    id=models.CharField(max_length=20,primary_key=True,null=False,unique=True)
-    title=models.CharField(max_length=100)
-    screen=models.CharField(max_length=20)
-    area=models.CharField(max_length=100)
-    theater=models.CharField(max_length=100)
+
+class movie(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    screen = models.CharField(max_length=20, null=True)
+    time = models.CharField(max_length=100, null=True)
+    img_src = models.CharField(max_length=100, null=True)
+    info = models.CharField(max_length=100, null=True)
+    update_date = models.DateField(max_length=100, null=True)
 
     def __str__(self):
         return self.title
--- search/views.py
+++ search/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from search.models import movies
+from search.models import movie
 import re
 import pandas as pd
 
@@ -7,50 +7,58 @@ import pandas as pd
 # Create your views here.
 
 
-def search(request,methods=['GET','POST']):
-    if request.method=='GET':
-        return render(request,'search.html')
+def search(request, methods=["GET", "POST"]):
+    if request.method == "GET":
+        return render(request, "search.html")
 
-    search=request.POST
-    searchDic={key:search[key] for key in search if search[key]!=""}
-    
+    search = request.POST
+    searchDic = {key: search[key] for key in search if search[key] != ""}
+
     ### 資料庫讀取全部資料
-    datas=movies.objects.all()
-    datas=[{'movieTitle':data.title,'movieScreen':data.screen,'area':data.area,'theater':data.theater} for data in datas]
+    datas = movie.objects.all()
+    datas = [
+        {
+            "movieTitle": data.title,
+            "movieScreen": data.screen,
+        }
+        for data in datas
+    ]
     ### DataFrame
-    df=pd.DataFrame(datas)
+    df = pd.DataFrame(datas)
 
     ### 搜尋欄無填寫視為全搜尋
-    if len(searchDic)==1:
-        results=datas
-        return render(request,'search.html',locals())
-    
+    if len(searchDic) == 1:
+        results = datas
+        return render(request, "search.html", locals())
+
     ### 針對螢幕篩選先重新建構字典
-    screens=['Imax','3D','digital']
-    searchDic['movieScreen']=[]
+    screens = ["Imax", "3D", "digital"]
+    searchDic["movieScreen"] = []
     for screen in screens:
         if screen in searchDic.keys():
-            searchDic['movieScreen'].append(screen)
+            searchDic["movieScreen"].append(screen)
             searchDic.pop(screen)
     print(searchDic)
 
     try:
         ### 電影標題搜尋
-        if 'movieTitle' in searchDic:
-            pattern=f"{searchDic['movieTitle']}"
-            df['movieTitle']=df['movieTitle'].map(lambda x: x if re.search(pattern, x) else None)
-            df=df.dropna()
+        if "movieTitle" in searchDic:
+            pattern = f"{searchDic['movieTitle']}"
+            df["movieTitle"] = df["movieTitle"].map(
+                lambda x: x if re.search(pattern, x) else None
+            )
+            df = df.dropna()
 
         ### DataFrame
-        for key in ["movieScreen","area","theater"]:
-            if key in searchDic and len(searchDic[key])!=0:
-                df[key]=df[key].map(lambda x: x if x in searchDic[key] else None)
-        datas=df.dropna()
-        datas=datas.to_dict('records')
+        for key in ["movieScreen", "theater"]:
+            if key in searchDic and len(searchDic[key]) != 0:
+                df[key] = df[key].map(lambda x: x if x in searchDic[key] else None)
+        datas = df.dropna()
+        datas = datas.to_dict("records")
         print(datas)
 
     except Exception as e:
         print(e)
-        datas=['error!']
-    
-    return render(request,'search.html',locals())
+        datas = ["error!"]
+
+    return render(request, "search.html", locals())
