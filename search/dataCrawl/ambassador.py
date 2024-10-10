9305205e61f97e858a23e59c9ea3d019f0a0c60c
PinLing <56640582+Pin-Ying@users.noreply.github.com>
2024-10-10 20:58:00 +0800

    finish ambassador connect to database

--- search/dataCrawl/ambassador.py
+++ search/dataCrawl/ambassador.py
@@ -25,7 +25,7 @@ def get_one_movie(url):
     # 影片類型
     types=sjcotyday[2].text.replace("影片類型：","") if sjcotyday[2].text.replace("影片類型：","") else "null"
     # 上映日期
-    upday=sjcotyday[3].text.replace("上映日期：","")if sjcotyday[3].text.replace("上映日期：","") else "null"
+    upday=sjcotyday[3].text.replace("上映日期：","").replace("/","-") if sjcotyday[3].text.replace("上映日期：","") else "null"
     time=soups.select('div.rating-box>span')
     # 電影時間
     if len(time) > 1:
@@ -52,7 +52,8 @@ def get_one_movie(url):
     mourl=soups.select('section>div>div>div>ul>li>ul>li>a')
     #https://www.ambassador.com.tw/
     for items in mourl:
-        date=items.get('href').split("DT=")[1]
+        dates=items.get('href').split("DT=")[1]
+        dates=[date.replace('/','-') for date in dates]
         urls="https://www.ambassador.com.tw"+items.get('href')
         soups=get_soup(urls)
         for item in soups.select('div.theater-box'):
@@ -61,7 +62,7 @@ def get_one_movie(url):
             seats=item.select("p>span.float-left.info")
             
             for time,seat in zip(times,seats):
-                show_infos.append([title,lcmove,date,time.text.strip(),seat.text])
+                show_infos.append([title,lcmove,dates,time.text.strip(),seat.text])
     finall.append(movie_datas)
     movietisr.extend(show_infos)
 
@@ -82,8 +83,8 @@ def get_movie_and_show():
         print(thread,'finish')
 
     movie_datas=pd.DataFrame(finall,columns=["電影名稱", "電影海報網址", "上或待上映","電影預告網址","影片類型","主要演員","電影介紹","電影時長","電影螢幕"])
-    movie_datas=pd.DataFrame(movietisr,columns=["電影名稱","影城","日期","時間", "廳位席位"])
-    return movie_datas,movie_datas
+    show_datas=pd.DataFrame(movietisr,columns=["電影名稱","影城","日期","時間", "廳位席位"])
+    return movie_datas,show_datas
     
 
 
@@ -96,78 +97,11 @@ def get_theater():
         title=items.find('h6').text.strip()
         address=items.find_all('p')[0].text.strip()
         phone=items.find_all('p')[1].text.strip()
-        theaters.append(["國賓影城",title,address,phone])
+        theaters.append([title,"國賓影城",address,phone])
     datas=pd.DataFrame(theaters,columns=["戲院名稱","影城", "影城位置", "影城電話"])
     return datas
     
 
-
-# # soup=get_soup('https://www.ambassador.com.tw/home/MovieList?Type=0')
-# soup=get_soup('https://www.ambassador.com.tw/home/MovieList?Type=0')
-# url=soup.select("div.cell>a.poster")
-# for i in url:
-#     urls="https://www.ambassador.com.tw"+i.get('href')
-#     soups=get_soup(urls)
-#     titles = soups.select('div.cell.small-12.medium-12.large-12.movie-info-box>h2')
-#     #titles[0].text電影名稱
-#     title=titles[0].text
-#     posturls=soups.select('div>img')
-#     #posturl[0].get('src')海報網址
-#     posturl=posturls[0].get('src')
-#     sjcotyday= soups.select('div.cell.small-12.medium-12.large-12.movie-info-box>p')
-#     #sjcotyday[0].text電影介紹
-#     sj=sjcotyday[0].text
-#     #sjcotyday[1].text主要演員：
-#     cost=sjcotyday[1].text.replace("主要演員：","") if sjcotyday[1].text.replace("主要演員：","") else "null"
-#     #sjcotyday[2].text影片類型：
-#     types=sjcotyday[2].text.replace("影片類型：","") if sjcotyday[2].text.replace("影片類型：","") else "null"
-#     #sjcotyday[3].text上映日期：
-#     upday=sjcotyday[3].text.replace("上映日期：","")if sjcotyday[3].text.replace("上映日期：","") else "null"
-#     time=soups.select('div.rating-box>span')
-#     #time[1].text電影時間
-#     if len(time) > 1:
-#         motime = time[1].text
-#     elif len(time) == 1:
-#         motime = time[0].text
-#     else:
-#         motime = "null"
-    
-#     seat=soups.select('div.theater-box>p.tag-seat')
-#     seats=set()
-#     for i in seat:
-#         if "數位" in i.text:
-#             seats.add("數位")
-#         if "3D" in i.text:
-#             seats.add("3D")
-#         if "IMAX" in i.text:
-#             seats.add("IMAX")
-#     seats=list(seats)
-#     finall.append([title,posturl,upday,'null',types,cost if cost else "null",
-#                    sj,motime,seats])
-    
-    # mourl=soups.select('section>div>div>div>ul>li>ul>li>a')
-    # #https://www.ambassador.com.tw/
-    # for items in mourl:     
-    #     urls="https://www.ambassador.com.tw"+items.get('href')
-    #     soups=get_soup(urls)
-    #     for item in soups.select('div.theater-box'):
-    #         lcmove=item.select("h3>a")
-    #         # lcmove[0].text 台北長春國賓影城
-    #         time=item.select("li>h6")
-    #         #.strip()
-    #         sets=item.select("p>span.float-left.info")
-    #         for i,j in zip(time,sets):
-    #             # print(i.text,j.text,lcmove[0].text)
-    #             movietisr.append([titles[0].text,lcmove[0].text,(items.text)[-10:],i.text.strip(),j.text])
-            
-    #         locs=item.select("div.theater-box>h3>span")
-    #         loc.add(("國賓影城",lcmove[0].text,locs[0].text,locs[2].text))
-
-# data=pd.DataFrame(movietisr,columns=["電影名稱","影城","日期","時間", "廳位席位"])
-# data.to_csv("gobimovieti.csv",index=False,)
-# data=pd.DataFrame(finall,columns=["電影名稱", "電影海報網址", "上或待上映","電影預告網址","影片類型","主要演員","電影介紹","電影時長","電影螢幕"])
-# data.to_csv("gobimovie.csv",index=False,)
-# locator=(By.ID,'recommend')
 
 if __name__=="__main__":
     print(get_movie_and_show())
